47aa39ac4d33ce05326184066e9ab1bd6ec043c1
souStypit <96959440+souStypit@users.noreply.github.com>
2022-11-30 20:38:01 +0300

    git commit

---
+++ Project 2 - Modal window/script.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const modalWindow = document.querySelector('.modal-window');
+const overlay = document.querySelector('.overlay');
+const btnCloseModalWindow = document.querySelector('.close-modal-window');
+const btnsShowModalWindow = document.querySelectorAll('.show-modal-window');
+
+const closeModalWindow = () => {
+  modalWindow.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const showModalWindow = () => {
+  modalWindow.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+btnsShowModalWindow.forEach((el) =>
+  el.addEventListener('click', showModalWindow)
+);
+
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+
+document.addEventListener('keyup')
+
+overlay.addEventListener('click', closeModalWindow);
